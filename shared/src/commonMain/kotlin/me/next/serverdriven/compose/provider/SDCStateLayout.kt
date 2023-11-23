88348bd7e3fa32055d964e99e8581ee78e58778c
vinicius <vinicius.sauter@hotmail.com>
2023-11-23 11:57:45 -0300

    SSD STATES LOG

--- shared/src/commonMain/kotlin/me/next/serverdriven/compose/provider/SDCStateLayout.kt
+++ shared/src/commonMain/kotlin/me/next/serverdriven/compose/provider/SDCStateLayout.kt
@@ -1,16 +1,10 @@
 package me.next.serverdriven.compose.provider
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import me.next.serverdriven.compose.SDCLibrary
 import me.next.serverdriven.compose.loadComponent
 import me.next.serverdriven.core.library.SDLibrary
@@ -48,18 +42,18 @@ fun SDCStateLayout(
     Column(modifier) {
         loadComponent(node = node, dataState = stateMap)
 //      TODO: remove states log
-//      TODO: verificar derivedStateOf para node.properties
+//      TODO: verify derivedStateOf for node.properties
 //        https://medium.com/androiddevelopers/jetpack-compose-when-should-i-use-derivedstateof-63ce7954c11b
-        Column(
-            Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Bottom
-        ) {
-            for (entry in stateMap) {
-                Text(
-                    modifier = Modifier.fillMaxWidth().background(color = Color.Yellow),
-                    text = "key: ${entry.key}, value: ${entry.value}"
-                )
-            }
-        }
+//        Column(
+//            Modifier.fillMaxSize(),
+//            verticalArrangement = Arrangement.Bottom
+//        ) {
+//            for (entry in stateMap) {
+//                Text(
+//                    modifier = Modifier.fillMaxWidth().background(color = Color.Yellow),
+//                    text = "key: ${entry.key}, value: ${entry.value}"
+//                )
+//            }
+//        }
     }
 }
