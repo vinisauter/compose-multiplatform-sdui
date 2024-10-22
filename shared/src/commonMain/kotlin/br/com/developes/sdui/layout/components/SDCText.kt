af3c1935d09fd8ff4ac24b96f4b36c10f9119b52
David Offrede <david.offrede@gmail.com>
2024-10-22 09:29:16 -0300

    Update Text

--- shared/src/commonMain/kotlin/br/com/developes/sdui/layout/components/SDCText.kt
+++ shared/src/commonMain/kotlin/br/com/developes/sdui/layout/components/SDCText.kt
@@ -1,17 +1,31 @@
 package br.com.developes.sdui.layout.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 import br.com.developes.sdui.ServerDrivenNode
 import br.com.developes.sdui.layout.Layout
 
 class SDCText(node: ServerDrivenNode, state: MutableMap<String, String>) : Layout {
-    private val modifier = Modifier.fromNode(node)
     val text by node.propertyState("text", state) { it ?: "" }
-
+    private val modifier = Modifier.fromNode(node)
+    private val fontSize = node.property("fontSize")?.toFloatOrNull()?.sp ?: 16.sp
+    private val color = node.property("color")?.hexToColor()
+    private val textAlign: TextAlign =
+        when (node.property("textAlign")) {
+            "Start" -> TextAlign.Start
+            "Center" -> TextAlign.Center
+            "End" -> TextAlign.End
+            "Justify" -> TextAlign.Justify
+            else -> TextAlign.Start
+        }
     private val fontWeight: FontWeight =
         when (node.property("fontWeight")) {
             null -> FontWeight.Normal
@@ -25,7 +39,10 @@ class SDCText(node: ServerDrivenNode, state: MutableMap<String, String>) : Layou
         Text(
             modifier = modifier,
             text = text,
-            fontWeight = fontWeight
+            fontWeight = fontWeight,
+            textAlign = textAlign,
+            fontSize = fontSize,
+            color = color ?: Color.White
         )
     }
 }
