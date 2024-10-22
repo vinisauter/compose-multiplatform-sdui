172fc0054c20ee9f9f156176367f7dc1ce6d2f4c
David Offrede <david.offrede@gmail.com>
2024-10-22 09:28:52 -0300

    Update TopAppBar

--- shared/src/commonMain/kotlin/br/com/developes/sdui/layout/components/SDCTopAppBar.kt
+++ shared/src/commonMain/kotlin/br/com/developes/sdui/layout/components/SDCTopAppBar.kt
@@ -1,9 +1,13 @@
 package br.com.developes.sdui.layout.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
@@ -14,6 +18,8 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import br.com.developes.sdui.SDCLibrary
 import br.com.developes.sdui.SDCLibrary.Companion.launchHandling
 import br.com.developes.sdui.ServerDrivenNode
@@ -27,6 +33,8 @@ class SDCTopAppBar(val node: ServerDrivenNode, val state: MutableMap<String, Str
     private val title by node.propertyState("title", state) { it ?: "" }
     private val actions = node.propertyNodes("onClick")
     private val enabled = node.propertyState("enabled", state)?.toBoolean()
+    private val backgroundColor = node.property("backgroundColor")?.hexToColor()
+    private val color = node.property("color")?.hexToColor()
 
     @Composable
     override fun Content() {
@@ -36,6 +44,7 @@ class SDCTopAppBar(val node: ServerDrivenNode, val state: MutableMap<String, Str
         val scope = rememberCoroutineScope()
 
         TopAppBar(
+            backgroundColor = backgroundColor ?: MaterialTheme.colors.primary,
             navigationIcon = {
                 IconButton(onClick = {
                     scope.launchHandling(after = { isEnabled = true }) {
@@ -44,18 +53,20 @@ class SDCTopAppBar(val node: ServerDrivenNode, val state: MutableMap<String, Str
                 }) {
                     Icon(
                         imageVector = vectorResource(Res.drawable.ic_back_button_toolbar),
-                        contentDescription = ""
+                        contentDescription = "",
+                        tint = color ?: Color.White
                     )
                 }
             },
             title = {
                 Box(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize().padding(end = 48.dp),
                     contentAlignment = Alignment.Center
                 ) {
                     Text(
-                        modifier =  modifier,
-                        text = title
+                        modifier = modifier,
+                        text = title,
+                        color = color ?: Color.White
                     )
                 }
             },
